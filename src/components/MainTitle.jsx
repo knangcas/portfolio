718f982d69801c062e9434ebe99262b6f8355815
knangcas <knangcas@asu.edu>
2025-04-03 13:47:08 -0500

    Added screenshots and links

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -58,7 +58,9 @@ export default function MainTitle() {
                 <h1 className="titleText2">P R O J E C T S</h1>
                 <div className="subTextI">
                     <p className="subSubText">PSYCHE SCRAPER</p>
-                    <p className="subSubTextI">
+                    <p className="subSubTextText">(In progress)</p>
+                    <p className="subSubTextText">C# - UNITY</p>
+                    <span className="subSubTextI">
                         &nbsp;&nbsp;&nbsp;&nbsp; Psyche Scraper is a university capstone project that spanned two
                         semesters (Fall 2024-Spring
                         2025)
@@ -70,29 +72,38 @@ export default function MainTitle() {
                         increase awareness of the <a target="_blank" href="https://psyche.asu.edu/">mission to
                         Psyche</a>. My team and I utilized the
                         <strong> AGILE SCRUM </strong> Software Development Lifecycle, developing the software
-                        in <strong>UNITY</strong> over several iterations. This
+                        in <strong>UNITY</strong> over several iterations. The <strong>C# programming
+                        language</strong> was used to code the logic of the game. This
                         project also involved composing high quality documents such as a project plan, quality plan,
                         and testing plan.
 
-                        PSYCHE SCRAPER involves playing as an astronaut in virtual reality. The astronaut has landed on the Psyche Asteroid and must
-                        collect metals on the asteroid to bring back to earth for study. The more metals collected, the more points are earned.
+                        PSYCHE SCRAPER involves playing as an astronaut in virtual reality. The astronaut has landed on
+                        the Psyche Asteroid and must
+                        collect metals on the asteroid to bring back to earth for study. The more metals collected, the
+                        more points are earned.
                         Player must collect as many metals as they can under a time limit.
                         <br/>
                         <br/>
-                        maybe 1 or 2 screenshots, then link to the repo? (it's private though).
-                    </p>
+                        <div className="screenshotContainer">
+                            <img className="screenshot" src="src/assets/PS3.png"/>
+                            <img className="screenshot" src="src/assets/PS2.png"/>
+                        </div>
+                    </span>
 
                     <br/>
                 </div>
                 <div className="subTextI">
                     <p className="subSubText">FlashCardsFX</p>
+                    <p className="subSubTextText">Java, JavaFX, MySQL</p>
                     <p className="subSubTextI">
                         &nbsp;&nbsp;&nbsp;&nbsp; FlashCards is a personal project first implemented in <strong>Java
                         using JavaFX</strong> making
                         use of a <strong>MySQL database</strong> and linking to Java
                         with a JDBC Driver. It also includes an "offline" mode which makes use of a JSON file to save
-                        data locally. The project is a application that is used to create "decks" of flashcards, which include a front and a back.
-                        The user can select a deck, then go through each card in the deck and study them. The user can "flip" the card to view the answer to a question.
+                        data locally. The project is a application that is used to create "decks" of flashcards, which
+                        include a front and a back.
+                        The user can select a deck, then go through each card in the deck and study them. The user can
+                        "flip" the card to view the answer to a question.
 
                         maybe 1 or 2 screenshots, then link to the repo? (it's private though).
                     </p>
@@ -101,7 +112,9 @@ export default function MainTitle() {
                 </div>
                 <div className="subTextI">
                     <p className="subSubText">FlashCards SPA</p>
-                    <p className="subSubTextI">
+                    <p className="subSubTextText"><a target="_blank" href="https://github.com/knangcas/SpringbootFlashcards">GitHub</a></p>
+                    <p className="subSubTextText">Docker - HTML - CSS - Javascript - Java SpringBoot - React - SQL</p>
+                    <span className="subSubTextI">
                         &nbsp;&nbsp;&nbsp;&nbsp; FlashCards SPA is a web-app version of FlashCardsFX
                         It was implemented as a web-application using <strong>javascript/html/css</strong> before
                         being re-implemented
@@ -109,16 +122,22 @@ export default function MainTitle() {
                         This web-app utilizes a <strong>RESTful API</strong> using <strong>Java
                         Springboot</strong> for the back-end technology.
                         <br/>
-
-
-                        maybe 1 or 2 screenshots, then link to the repo? (it's private though).
-                    </p>
-
+                        <br/>
+                        <div className="screenshotContainer">
+                        <img className="screenshot" src="src/assets/SBFC1.png"/>
+                        <img className="screenshot" src="src/assets/SBFC2.png"/>
+                        </div>
+                    </span>
+                    <br/>
 
                 </div>
 
                 <div className="subTextI">
                     <p className="subSubText">FlashCards ReactNative</p>
+                    <p className="subSubTextText"><a target="_blank"
+                                                     href="https://github.com/knangcas/flashcardsReactNative">GitHub</a>
+                    </p>
+                    <p className="subSubTextText">HTML - CSS - Javascript - Java SpringBoot - ReactNative - SQL</p>
                     <p className="subSubTextI">
                         &nbsp;&nbsp;&nbsp;&nbsp; FlashCards ReactNative is a mobile-app version of FlashCardsFX.
                         Implemented using <strong>ReactNative using Expo</strong>, the aim of this project was to learn
@@ -135,9 +154,13 @@ export default function MainTitle() {
 
                 <div className="subTextI">
                     <p className="subSubText">ConnectFourFX</p>
+                    <p className="subSubTextText"><a target="_blank"
+                                                     href="https://github.com/knangcas/connectFour">GitHub</a>
+                    </p>
+                    <p className="subSubTextText">Java - JavaFX</p>
                     <p className="subSubTextI">
                         &nbsp;&nbsp;&nbsp;&nbsp; ConnectFourFX is a implementation of the popular game "ConnectFour" in
-                        <strong>Java using JavaFX</strong> Utilizing the scene builder tool in JavaFX, a recreation of
+                        <strong> Java using JavaFX</strong> Utilizing the scene builder tool in JavaFX, a recreation of
                         the board from Connect Four was created.
 
 
@@ -147,11 +170,19 @@ export default function MainTitle() {
 
                 <div className="subTextI">
                     <p className="subSubText">JeopardyVue</p>
+                    <p className="subSubTextText"><a target="_blank"
+                                                     href="https://github.com/knangcas/JeopardyVue">GitHub</a>
+                    </p>
+                    <p className="subSubTextText">HTML - CSS - Javascript - VueJS</p>
                     <p className="subSubTextI">
-                        &nbsp;&nbsp;&nbsp;&nbsp; JeopardyVue was a class project from the class "web-based applications" taken at
-                        Arizona State University. In this project, a recreation of the Jeopardy game was created using the options API of the
-                        <strong>VueJS framework</strong>. This involved making api calls using the fetch API to a trivia database for the questions/answers,
-                        as well as making a dynamic application that allowed for a variable amount of players and categories.
+                        &nbsp;&nbsp;&nbsp;&nbsp; JeopardyVue was a class project from the class "web-based applications"
+                        taken at
+                        Arizona State University. In this project, a recreation of the Jeopardy game was created using
+                        the options API of the
+                        <strong>VueJS framework</strong>. This involved making api calls using the fetch API to a trivia
+                        database for the questions/answers,
+                        as well as making a dynamic application that allowed for a variable amount of players and
+                        categories.
 
 
                         maybe 1 or 2 screenshots, then link to the repo? (it's private though).
